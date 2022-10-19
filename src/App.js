69635cd65f0be1e6cf93d1c6a0ab2217de31539e
Jonathan <Jonathanxl1@hotmail.com>
2022-10-19 12:17:56 -0500

    Refactor code by use css styles

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import Square from "./components/square"
 function App() {
   
   return (
-    <div className="App">
-      <div className="first_row" >
-        {Array(3).fill(null).map((ele,idx)=>{
-          return  (<Square className="square" key={idx} >{idx+1}</Square>)
+    <div className="desktop">
+      <div className="container">
+      <div className="columns is-desktop mt-5 ml-5" >
+        <div className="column is-12" style={{height:'600px'}}>
+        {Array(9).fill(null).map((ele,idx)=>{
+          return  (<Square  key={idx} >{idx+1}</Square>)
         })}
+        </div>
+        
       </div>
-      <div className="second_row">
-      {Array(3).fill(null).map((ele,idx)=>{
-          return  (<Square className="square" key={idx} >{idx+3}</Square>)
-        })}
-      </div>
-      <div className="third_row">
-      {Array(3).fill(null).map((ele,idx)=>{
-          return  (<Square className="square" key={idx} >{idx+6}</Square>)
-        })}
-      </div>
+    </div>
     </div>
   );
 }
